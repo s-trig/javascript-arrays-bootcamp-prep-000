a4b834c8da09d9d60e1c5c36dca22beb64959941
David Reep <davereep@gmail.com>
2017-09-18 19:58:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
+<<<<<<< HEAD
   const newar = [element, ...array];
+=======
+  var newar = [...array, element];
+>>>>>>> df10a0fd415f830fe0d88eaad06c65f659425d2f
   return newar
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+<<<<<<< HEAD
   return array
 }
 
@@ -41,4 +46,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   const newar = array.slice(0, array.length-1)
   return newar
+=======
+>>>>>>> df10a0fd415f830fe0d88eaad06c65f659425d2f
 }
